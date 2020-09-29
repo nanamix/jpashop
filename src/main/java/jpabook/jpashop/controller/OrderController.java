abe0612656ceb998bf4cc053602ad8d3e5a3e1c6
nanamix <jyha81@gmail.com>
2020-09-30 00:54:51 +0900

    2020-09-30-5

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -34,6 +36,13 @@ public class OrderController {
 
     }
 
+    @PostMapping("/order")
+    public String Order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itmeId") Long itemId,
+                        @RequestParam("count") int count){
+        orderService.order(memberId,itemId,count);
 
+        return "redirect:/orders";
+    }
 
 }
