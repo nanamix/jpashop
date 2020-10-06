23937ba94bf3ca5270e0476bdd328af7c07f768d
nanamix <jyha81@gmail.com>
2020-10-06 22:02:18 +0900

    2020-10-06-3

--- src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderItemQueryDto.java
@@ -4,7 +4,18 @@ import lombok.Data;
 
 @Data
 public class OrderItemQueryDto {
+
+
+    private Long orderId;
     private String itemName;
     private int OrderPrice;
     private int count;
+
+
+    public OrderItemQueryDto(Long orderId, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.itemName = itemName;
+        OrderPrice = orderPrice;
+        this.count = count;
+    }
 }
